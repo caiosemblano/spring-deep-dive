ec83c7086d1606c663c65244a4d0829e418f1204
Caio Semblano Silva <semblanocaio@gmail.com>
2026-03-06 18:27:03 -0300

    First commit

---
+++ src/main/java/com/learning/spring_deep_dive/SpringDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring_deep_dive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeepDiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/spring_deep_dive/SpringDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring_deep_dive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeepDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
